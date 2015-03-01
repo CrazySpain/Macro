5e440ca9a24872fbe1800da70c2fbba2608ce4a8
CrazySpain <toraholdman@gmail.com>
2015-03-01 17:57:30 +0200

    Initial commit.

---
+++ Macro/main.c
@@ -0,0 +1,27 @@
+//
+//  main.c
+//  Macro
+//
+//  Created by Tora on 2/18/15.
+//  Copyright (c) 2015 Tora. All rights reserved.
+//
+
+#include <stdio.h>
+#include "TGMacroTest.h"
+
+int main(int argc, const char * argv[]) {
+    TGMacroTest();
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
